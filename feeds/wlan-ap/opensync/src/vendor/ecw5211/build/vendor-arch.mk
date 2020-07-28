605a3b89d406e4e4e37489f45e928364af9a42f0
John Crispin <john@phrozen.org>
2020-07-28 15:59:02 +0200

    ecw5211: make the board use the unified opensync target
    Drop the vendor specific target. Also fix ct-fwcfg for this board.
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/wlan-ap/opensync/src/vendor/ecw5211/build/vendor-arch.mk
+++
@@ -1,9 +0,0 @@
-OS_TARGETS +=ECW5211
-
-ifeq ($(TARGET),ECW5211)
-PLATFORM=openwrt
-VENDOR=ecw5211
-PLATFORM_DIR := platform/$(PLATFORM)
-KCONFIG_TARGET ?= $(PLATFORM_DIR)/kconfig/openwrt_generic
-ARCH_MK := $(PLATFORM_DIR)/build/$(PLATFORM).mk
-endif
