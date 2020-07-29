dab51c9bf0aa7f1b7c81231feb2f1e906c827d7c
John Crispin <john@phrozen.org>
2020-07-29 12:06:16 +0200

    opensync: more unified target updates
    convert ap2220 and ea8300 to use the unified package.
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/wlan-ap/opensync/src/vendor/ipq40xx/build/vendor-arch.mk
+++
@@ -1,9 +0,0 @@
-OS_TARGETS +=IPQ40XX
-
-ifeq ($(TARGET),IPQ40XX)
-PLATFORM=openwrt
-VENDOR=ipq40xx
-PLATFORM_DIR := platform/$(PLATFORM)
-KCONFIG_TARGET ?= $(PLATFORM_DIR)/kconfig/openwrt_generic
-ARCH_MK := $(PLATFORM_DIR)/build/$(PLATFORM).mk
-endif
