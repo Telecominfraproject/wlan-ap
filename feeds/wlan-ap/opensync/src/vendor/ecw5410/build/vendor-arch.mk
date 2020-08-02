eb2281b321983923168975462cef13a56284173b
John Crispin <john@phrozen.org>
2020-08-02 14:52:56 +0200

    opensync: convert the last board to the unified package
    There is no a single opensync package that reads the board data from uci.
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/wlan-ap/opensync/src/vendor/ecw5410/build/vendor-arch.mk
+++
@@ -1,9 +0,0 @@
-OS_TARGETS +=ECW5410
-
-ifeq ($(TARGET),ECW5410)
-PLATFORM=openwrt
-VENDOR=ecw5410
-PLATFORM_DIR := platform/$(PLATFORM)
-KCONFIG_TARGET ?= $(PLATFORM_DIR)/kconfig/openwrt_generic
-ARCH_MK := $(PLATFORM_DIR)/build/$(PLATFORM).mk
-endif
