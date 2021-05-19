da5b22e3a6fd6560b6382fe14d7d5e3d1818e89b
Max Brenner <xamrennerb@gmail.com>
2021-05-19 11:32:23 +0200

    add device ID file check
    Signed-off-by: Max Brenner <xamrennerb@gmail.com>

--- feeds/wlan-ap/opensync/files/bin/wlan_ap_redirector.sh
+++ feeds/wlan-ap/opensync/files/bin/wlan_ap_redirector.sh
@@ -12,6 +12,11 @@ fi
 
 # Query DigiCert's API if redirector wasn't specified
 if [ -z "$1" ]; then
+  if [ ! -f "$AP_DEVICE_ID_FILE" ]; then
+      echo "Device ID file $AP_DEVICE_ID_FILE does not exist. Make sure to create it or specify the redirector address manually."
+      exit 1
+  fi
+
   # TODO: this command should be retried if it fails
   digicert_device_id=`cat ${AP_DEVICE_ID_FILE}`
   device_data=`curl -s \
