d13b4c0f984e75d83882b07992a6d54eae56ca7a
Rick Sommerville <rick.sommerville@netexperience.com>
2021-05-22 18:26:56 -0400

    WIFI-2416  Automatically DigiCert if no redirector setup
    Signed-off-by: Rick Sommerville <rick.sommerville@netexperience.com>

--- feeds/wlan-ap/opensync/files/bin/wlan_ap_redirector.sh
+++ feeds/wlan-ap/opensync/files/bin/wlan_ap_redirector.sh
@@ -25,12 +25,21 @@ if [ -z "$1" ]; then
     "https://${DIGICERT_API_URI}/iot/api/v2/device/${digicert_device_id}"`
 
   controller_url=`echo ${device_data} | jsonfilter -e '@.fields[@.name="Redirector"].value'`
-  # TODO: we should get the port with the redirector record and only default to 6643 if no port was specified
-  redirector_addr="ssl:${controller_url}:6643"
+  if [ -z "$controller_url" ]; then
+    echo "No redirector found for this device"
+    exit 1
+  fi
+  controller_port=`echo ${controller_url} | cut -d ":" -f2)`
+  if [ -z "$controller_port" ]; then
+    redirector_addr="ssl:${controller_url}:6643"
+  else
+    redirector_addr="ssl:${controller_url}"
+  fi
 else
   redirector_addr=$1
 fi
 
 uci set system.tip.redirector="${redirector_addr}"
+uci set system.tip.deployed=0
 uci commit system
 /etc/init.d/opensync restart
