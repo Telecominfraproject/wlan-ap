c094045a26fe634b7e59af4e4384e074c1106c35
Nagendrababu <nagendrababu.bonkuri@connectus.ai>
2021-06-08 17:36:17 -0400

    WIFI-1934-LED-Turnoff
    This Patch will add support to turnoff LEDs of the AP through cloud
    command
    
    Signed-off-by: Nagendrababu <nagendrababu.bonkuri@connectus.ai>

--- feeds/wlan-ap/opensync/files/bin/wlan_ap_led.sh
+++ feeds/wlan-ap/opensync/files/bin/wlan_ap_led.sh
@@ -2,3 +2,6 @@
 
 #Blink AP's LED
 /usr/opensync/tools/ovsh insert Node_Config module:="led" key:="led_blink" value:="on"
+
+#Turnoff AP's LED
+/usr/opensync/tools/ovsh insert Node_Config module:="led" key:="led_off" value:="off"
