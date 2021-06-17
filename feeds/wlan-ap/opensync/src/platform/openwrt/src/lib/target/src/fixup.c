d166cf1e31ce7e9b9877154a7f3edc52fcf815a5
Chaitanya Godavarthi <chaitanya.kiran@netexperience.com>
2021-06-16 21:56:11 -0400

    wifi-2649 Fix Captive portal vifC vifS mismatch
    Mismatch in vifC and vifS for captive portal is
    leading to opennds restarts and memory leakage.
    Fix by making sure the vifC and vifS are in parity.
    
    uci_blob not being freed, fix by making uci_blob
    as global so that it need not be freed and can
    be reused instead.
    
    Signed-off-by: Chaitanya Godavarthi <chaitanya.kiran@netexperience.com>
    
    Cleaning up captive portal code to improve stability and reduce memory leaks
    
    Signed-off-by: Nagendrababu <nagendrababu.bonkuri@connectus.ai>

---
+++ feeds/wlan-ap/opensync/src/platform/openwrt/src/lib/target/src/fixup.c
@@ -0,0 +1,89 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include "log.h"
+#include "const.h"
+#include "target.h"
+
+#include <libubox/avl-cmp.h>
+#include <libubox/avl.h>
+#include <libubox/vlist.h>
+#include <net/if.h>
+
+#include "fixup.h"
+
+
+/*
+ * VIF Fixup
+ */
+
+static struct avl_tree vif_fixup_tree = AVL_TREE_INIT(vif_fixup_tree, avl_strcmp, false, NULL);
+
+struct vif_fixup * vif_fixup_find(const char *ifname)
+{
+        struct vif_fixup *vif = avl_find_element(&vif_fixup_tree, ifname, vif, avl);
+        if (vif)
+                return vif;
+
+	/* Not found, add */
+        vif = malloc(sizeof(*vif));
+        if (!vif)
+                return NULL;
+
+        memset(vif, 0, sizeof(*vif));
+        strncpy(vif->name, ifname, IF_NAMESIZE);
+        vif->avl.key = vif->name;
+        avl_insert(&vif_fixup_tree, &vif->avl);
+        return vif;
+}
+
+void vif_fixup_del(char *ifname)
+{
+        struct vif_fixup *vif = NULL;
+        vif = avl_find_element(&vif_fixup_tree, ifname, vif, avl);
+        if (vif) {
+		avl_delete(&vif_fixup_tree, &vif->avl);
+		free(vif);
+	}
+}
+
+bool vif_fixup_captive_enabled(void)
+{
+        struct vif_fixup *vif_ptr = NULL;
+        struct vif_fixup *vif = NULL;
+
+	avl_for_each_element_safe(&vif_fixup_tree, vif, avl, vif_ptr) {
+		if (vif->has_captive == true)
+			return true;
+	}
+	return false;
+}
+
+bool vif_fixup_iface_captive_enabled(const char *ifname)
+{
+	struct vif_fixup * vif = NULL;
+
+	vif = vif_fixup_find(ifname);
+
+	if (vif)
+		return vif->has_captive;
+	else
+		return false;
+}
+
+void vif_fixup_set_iface_captive(const char *ifname, bool en)
+{
+	struct vif_fixup * vif = NULL;
+
+	vif = vif_fixup_find(ifname);
+
+	if (vif)
+		vif->has_captive = en;
+}
