319e7e93bd522ee158b7c7aa2273d145278f0c72
Nagendrababu <nagendrababu.bonkuri@connectus.ai>
2021-11-01 14:45:48 -0400

    WIFI-5291-APC-config-reload
    This patch will fix the config reload issue when DR and BDR addresses
    change
    
    Signed-off-by: Nagendrababu <nagendrababu.bonkuri@connectus.ai>

--- feeds/wlan-ap/opensync/src/platform/openwrt/src/lib/target/src/radio.c
+++ feeds/wlan-ap/opensync/src/platform/openwrt/src/lib/target/src/radio.c
@@ -884,15 +884,8 @@ static void callback_APC_State(ovsdb_update_monitor_t *mon,
 	     (conf->bdr_addr_changed)? "changed":"unchanged",
 	     (conf->mode_changed)? "changed":"unchanged");
 
-	/* APC changed: if radproxy enabled then restart wireless */
-	if (radproxy_apc) {
-		radproxy_apc = 0;
-		system("ubus call service event '{\"type\": \"config.change\", \"data\": { \"package\": \"wireless\" }}'");
-	}
-
 	/* APC changed: start / stop radius proxy service if needed */
 	vif_check_radius_proxy();
-
 }
 
 struct schema_APC_State apc_state;
@@ -1046,7 +1039,10 @@ void apc_state_set(struct blob_attr *msg)
 
 	if (!ovsdb_table_update(&table_APC_State, &apc_state))
 		LOG(ERR, "APC_state: failed to update");
-
+	if (radproxy_apc) {
+		radproxy_apc = 0;
+		system("ubus call service event '{\"type\": \"config.change\", \"data\": { \"package\": \"wireless\" }}'");
+	}
 }
 
 static ovsdb_table_t table_Manager;
