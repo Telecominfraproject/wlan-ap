806198895198b3d4d851ab4033fa9fe963a1a30c
John Crispin <john@phrozen.org>
2022-01-02 14:29:52 +0100

    mediatek: add SDK target
    This adds support for panther and merlin.
    
    Signed-off-by: John Crispin <john@phrozen.org>

---
+++ feeds/mediatek-sdk/mediatek-sdk/mt7981/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,45 @@
+platform_do_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	*snand*)
+		nand_do_upgrade "$1"
+		;;
+	*)
+		default_do_upgrade "$1"
+		;;
+	esac
+}
+
+PART_NAME=firmware
+
+platform_check_image() {
+	local board=$(board_name)
+	local magic="$(get_magic_long "$1")"
+
+	[ "$#" -gt 1 ] && return 1
+
+	case "$board" in
+	*snand*)
+		# tar magic `ustar`
+		magic="$(dd if="$1" bs=1 skip=257 count=5 2>/dev/null)"
+
+		[ "$magic" != "ustar" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+
+		return 0
+		;;
+	*)
+		[ "$magic" != "d00dfeed" ] && {
+			echo "Invalid image type."
+			return 1
+		}
+		return 0
+		;;
+	esac
+
+	return 0
+}
+
