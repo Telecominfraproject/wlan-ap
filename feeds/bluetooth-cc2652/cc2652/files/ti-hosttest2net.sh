344c19b1a6498145727538ceb20355465103bc26
John Crispin <john@phrozen.org>
2022-09-18 06:39:47 +0200

    treewide: update to owrt 22.03
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/bluetooth-cc2652/cc2652/files/ti-hosttest2net.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-killall -9 ser2net
-tisbl.sh /dev/ttyMSM1 115200 2652 /etc/tifirmware/ble5_host_test_bd9.bin 79 34
-echo "7000:telnet:0:/dev/ttyMSM1:115200 8DATABITS NONE 1STOPBIT remctl" > /tmp/ser2net.conf
-ser2net -c /tmp/ser2net.conf
