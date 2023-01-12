639edf36a7c2d2725d9834cd6f3556a7df639cee
John Crispin <john@phrozen.org>
2023-01-12 15:03:21 +0100

    ipq807x: more fixes
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
+++ feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
@@ -27,15 +27,6 @@ define Device/edgecore_oap101_6e
 endef
 TARGET_DEVICES += edgecore_oap101_6e
 
-define Device/liteon_wpx8324
-  DEVICE_TITLE := Liteon WPX8324
-  DEVICE_DTS := qcom-ipq5018-liteon-wpx8324
-  SUPPORTED_DEVICES := liteon,wpx8324
-  DEVICE_PACKAGES := ath11k-wifi-liteon-wpx8324 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122
-  DEVICE_DTS_CONFIG := config@mp03.5-c1
-endef
-#TARGET_DEVICES += liteon_wpx8324
-
 define Device/muxi_ap3220l
   DEVICE_TITLE := MUXI AP3220L
   DEVICE_DTS := qcom-ipq5018-muxi-ap3220l
