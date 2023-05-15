e6ed1743d61ebde80e3124243184986f0a0abe2d
John Crispin <john@phrozen.org>
2023-05-15 09:39:53 +0200

    ipq807x: turn bootconfig into a module
    Fixes: WIFI-12537
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ipq807x/ipq807x/modules.mk
+++ feeds/ipq807x/ipq807x/modules.mk
@@ -130,3 +130,17 @@ endef
 
 
 $(eval $(call KernelPackage,usb-dwc3-qcom))
+
+define KernelPackage/bootconfig
+  SUBMENU:=Other modules
+  TITLE:=Bootconfig partition for failsafe
+  KCONFIG:=CONFIG_BOOTCONFIG_PARTITION
+  FILES:=$(LINUX_DIR)/drivers/platform/ipq/bootconfig.ko@ge4.4
+  AUTOLOAD:=$(call AutoLoad,56,bootconfig,1)
+endef
+
+define KernelPackage/bootconfig/description
+  Bootconfig partition for failsafe
+endef
+
+$(eval $(call KernelPackage,bootconfig))
