2cb80340befcd72b9a536ce71d8e8482c9709308
John Crispin <john@phrozen.org>
2023-08-08 19:22:42 +0200

    23.05: various ipq807x_v5.4 fixes
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ipq807x_v5.4/ipq50xx/base-files/lib/upgrade/platform.sh
+++ feeds/ipq807x_v5.4/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -5,19 +5,19 @@ RAMFS_COPY_BIN='fw_setenv'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock /tmp/downgrade'
 
 qca_do_upgrade() {
-        local tar_file="$1"
+	local tar_file="$1"
 
-        local board_dir=$(tar tf $tar_file | grep -m 1 '^sysupgrade-.*/$')
-        board_dir=${board_dir%/}
+	local board_dir=$(tar tf $tar_file | grep -m 1 '^sysupgrade-.*/$')
+	board_dir=${board_dir%/}
 	local dev=$(find_mtd_chardev "0:HLOS")
 
-        tar Oxf $tar_file ${board_dir}/kernel | mtd write - ${dev}
+	tar Oxf $tar_file ${board_dir}/kernel | mtd write - ${dev}
 
-        if [ -n "$UPGRADE_BACKUP" ]; then
-                tar Oxf $tar_file ${board_dir}/root | mtd -j "$UPGRADE_BACKUP" write - rootfs
-        else
-                tar Oxf $tar_file ${board_dir}/root | mtd write - rootfs
-        fi
+	if [ -n "$UPGRADE_BACKUP" ]; then
+		tar Oxf $tar_file ${board_dir}/root | mtd -j "$UPGRADE_BACKUP" write - rootfs
+	else
+		tar Oxf $tar_file ${board_dir}/root | mtd write - rootfs
+	fi
 }
 
 find_mmc_part() {
@@ -59,50 +59,20 @@ emmc_do_upgrade() {
 	do_flash_emmc $tar_file 'rootfs' $board_dir root
 
 	local emmcblock="$(find_mmc_part "rootfs_data")"
-        if [ -e "$emmcblock" ]; then
-                mkfs.ext4 -F "$emmcblock"
-        fi
+	if [ -e "$emmcblock" ]; then
+		mkfs.ext4 -F "$emmcblock"
+	fi
 }
 
 platform_check_image() {
 	local magic_long="$(get_magic_long "$1")"
 	board=$(board_name)
 	case $board in
-	cig,wf188|\
-	cig,wf188n|\
-	cig,wf194c|\
-	cig,wf194c4|\
-	cig,wf196|\
-	cybertan,eww622-a1|\
-	glinet,ax1800|\
-	glinet,axt1800|\
-	indio,um-310ax-v1|\
-	indio,um-510axp-v1|\
-	indio,um-510axm-v1|\
-	wallys,dr6018|\
-	wallys,dr6018-v4|\
-	edgecore,eap101|\
-	edgecore,eap102|\
 	edgecore,eap104|\
+	hfcl,ion4xi_w|\
+	yuncore,fap655|\	
 	edgecore,oap101-6e|\
-	edgecore,oap101e|\
-	edgecore,eap106|\
-	hfcl,ion4xi|\
-	hfcl,ion4x|\
-	hfcl,ion4x_2|\
-	hfcl,ion4xe|\
-	muxi,ap3220l|\
-	plasmacloud,pax1800-v1|\
-	plasmacloud,pax1800-v2|\
-	tplink,ex227|\
-	tplink,ex447|\
-	yuncore,ax840|\
-	motorola,q14|\
-	muxi,ap3220l|\
-	qcom,ipq6018-cp01|\
-	qcom,ipq807x-hk01|\
-	qcom,ipq807x-hk14|\
-	qcom,ipq5018-mp03.3)
+	edgecore,oap101e)
 		[ "$magic_long" = "73797375" ] && return 0
 		;;
 	esac
@@ -116,79 +86,33 @@ platform_do_upgrade() {
 
 	board=$(board_name)
 	case $board in
-	cig,wf188)
-		qca_do_upgrade $1
-		;;
-	motorola,q14)
-		emmc_do_upgrade $1
-		;;
-	cig,wf188n|\
-	cig,wf194c|\
-	cig,wf194c4|\
-	cig,wf196|\
-	cybertan,eww622-a1|\
-	glinet,ax1800|\
-	glinet,axt1800|\
-	indio,um-310ax-v1|\
-	indio,um-510axp-v1|\
-	indio,um-510axm-v1|\
-	qcom,ipq6018-cp01|\
-	qcom,ipq807x-hk01|\
-	qcom,ipq807x-hk14|\
-	qcom,ipq5018-mp03.3|\
-	wallys,dr6018|\
-	wallys,dr6018-v4|\
-	yuncore,ax840|\
-	tplink,ex447|\
-	tplink,ex227|\
 	edgecore,oap101-6e|\
-	edgecore,oap101e|\
-	meshpp,s618-cp03|\
-	meshpp,s618-cp01)
+	edgecore,oap101e)
 		nand_upgrade_tar "$1"
 		;;
-	hfcl,ion4xi|\
-	hfcl,ion4x|\
-	hfcl,ion4x_2|\
-	hfcl,ion4xe)
-		if grep -q rootfs_1 /proc/cmdline; then
-			CI_UBIPART="rootfs"
-			fw_setenv primary 0 || exit 1
-		else
-			CI_UBIPART="rootfs_1"
-			fw_setenv primary 1 || exit 1
-		fi
-		nand_upgrade_tar "$1"
-		;;
-	edgecore,eap104|\
-	edgecore,eap106)
+	edgecore,eap104)
 		CI_UBIPART="rootfs1"
 		[ "$(find_mtd_chardev rootfs)" ] && CI_UBIPART="rootfs"
 		nand_upgrade_tar "$1"
 		;;
-	edgecore,eap101|\
-	edgecore,eap102)
-		if [ "$(find_mtd_chardev rootfs)" ]; then
-			CI_UBIPART="rootfs"
-		else
-			if [ -e /tmp/downgrade ]; then
-				CI_UBIPART="rootfs1"
-				fw_setenv active 1 || exit 1
-				fw_setenv upgrade_available 0 || exit 1
-			elif grep -q rootfs1 /proc/cmdline; then
-				CI_UBIPART="rootfs2"
-				fw_setenv active 2 || exit 1
-			else
-				CI_UBIPART="rootfs1"
-				fw_setenv active 1 || exit 1
-			fi
-		fi
+        hfcl,ion4xi_w)
+                wp_part=$(fw_printenv primary | cut  -d = -f2)
+                echo "Current Primary is $wp_part"
+                if [[ $wp_part == 1 ]]; then
+                        CI_UBIPART="rootfs"
+                        CI_FWSETENV="primary 0"
+                else
+                        CI_UBIPART="rootfs_1"
+                        CI_FWSETENV="primary 1"
+                fi
+                nand_upgrade_tar "$1"
+                ;;
+	yuncore,fap655)
+		[ -f /proc/boot_info/rootfs/upgradepartition ] && {
+			CI_UBIPART="$(cat /proc/boot_info/rootfs/upgradepartition)"
+			CI_BOOTCFG=1
+		}
 		nand_upgrade_tar "$1"
-		;;
-	plasmacloud,pax1800-v1|\
-	plasmacloud,pax1800-v2)
-		PART_NAME="inactive"
-		platform_do_upgrade_dualboot_datachk "$1"
 		;;
 	esac
 }
--- feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
+++ feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
@@ -1,13 +1,5 @@
 KERNEL_LOADADDR := 0x41080000
 
-define Device/cybertan_eww622_a1
-  DEVICE_TITLE := CyberTan EWW622-A1
-  DEVICE_DTS := qcom-ipq5018-eww622-a1
-  SUPPORTED_DEVICES := cybertan,eww622-a1
-  DEVICE_PACKAGES := ath11k-wifi-cybertan-eww622-a1 ath11k-firmware-ipq50xx ath11k-firmware-qcn9000
-  DEVICE_DTS_CONFIG := config@mp03.1
-endef
-#TARGET_DEVICES += cybertan_eww622_a1
 
 define Device/edgecore_eap104
   DEVICE_TITLE := EdgeCore EAP104
@@ -18,11 +10,20 @@ define Device/edgecore_eap104
 endef
 TARGET_DEVICES += edgecore_eap104
 
+define Device/yuncore_fap655
+  DEVICE_TITLE := Yuncore FAP650
+  DEVICE_DTS := qcom-ipq5018-yuncore-fap655
+  SUPPORTED_DEVICES := yuncore,fap655
+  DEVICE_PACKAGES := ath11k-wifi-yuncore-fap655 ath11k-firmware-ipq50xx-map-spruce -kmod-usb-dwc3-of-simple kmod-usb-dwc3-qcom kmod-usb3
+  DEVICE_DTS_CONFIG := config@mp03.3
+endef
+TARGET_DEVICES += yuncore_fap655
+
 define Device/edgecore_oap101_6e
   DEVICE_TITLE := EdgeCore OAP101 6E
   DEVICE_DTS := qcom-ipq5018-oap101-6e
   SUPPORTED_DEVICES := edgecore,oap101-6e
-  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-hwmon-tmp102 kmod-gpio-pca953x ugps kmod-tpm-tis-i2c
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-gpio-pca953x ugps
   DEVICE_DTS_CONFIG := config@mp03.5-c1
 endef
 TARGET_DEVICES += edgecore_oap101_6e
@@ -31,45 +32,16 @@ define Device/edgecore_oap101e
   DEVICE_TITLE := EdgeCore OAP101 E
   DEVICE_DTS := qcom-ipq5018-oap101e
   SUPPORTED_DEVICES := edgecore,oap101e
-  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-hwmon-tmp102 kmod-gpio-pca953x ugps kmod-tpm-tis-i2c
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-gpio-pca953x ugps
   DEVICE_DTS_CONFIG := config@mp03.5-c1
 endef
 TARGET_DEVICES += edgecore_oap101e
 
-define Device/muxi_ap3220l
-  DEVICE_TITLE := MUXI AP3220L
-  DEVICE_DTS := qcom-ipq5018-muxi-ap3220l
-  SUPPORTED_DEVICES := muxi,ap3220l
-  DEVICE_PACKAGES := ath11k-wifi-muxi-ap3220l ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122
-  DEVICE_DTS_CONFIG := config@mp03.5-c1
-endef
-#TARGET_DEVICES += muxi_ap3220l
-
-define Device/motorola_q14
-  DEVICE_TITLE := Motorola Q14
-  DEVICE_DTS := qcom-ipq5018-q14
-  SUPPORTED_DEVICES := motorola,q14
-  DEVICE_PACKAGES := ath11k-wifi-motorola-q14 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122
-  DEVICE_DTS_CONFIG := config@mp03.5-c1
-  IMAGES := sysupgrade.tar mmc-factory.bin
-  IMAGE/mmc-factory.bin := append-ubi | qsdk-ipq-factory-mmc
-endef
-#TARGET_DEVICES += motorola_q14
-
-define Device/qcom_mp03_1
-  DEVICE_TITLE := Qualcomm Maple 03.1
-  DEVICE_DTS := qcom-ipq5018-mp03.1
-  SUPPORTED_DEVICES := qcom,ipq5018-mp03.1
-  DEVICE_PACKAGES := ath11k-wifi-qcom-ipq5018
-  DEVICE_DTS_CONFIG := config@mp03.1
-endef
-#TARGET_DEVICES += qcom_mp03_1
-
-define Device/qcom_mp03_3
-  DEVICE_TITLE := Qualcomm Maple 03.3
-  DEVICE_DTS := qcom-ipq5018-mp03.3
-  SUPPORTED_DEVICES := qcom,ipq5018-mp03.3
-  DEVICE_PACKAGES := ath11k-wifi-qcom-ipq5018
+define Device/hfcl_ion4xi_w
+  DEVICE_TITLE := HFCL ION4xi_w
+  DEVICE_DTS := qcom-ipq5018-hfcl-ion4xi_w
+  SUPPORTED_DEVICES := hfcl,ion4xi_w
+  DEVICE_PACKAGES := ath11k-wifi-hfcl-ion4xi_w ath11k-firmware-ipq50xx-map-spruce
   DEVICE_DTS_CONFIG := config@mp03.3
 endef
-#TARGET_DEVICES += qcom_mp03_3
+TARGET_DEVICES += hfcl_ion4xi_w
