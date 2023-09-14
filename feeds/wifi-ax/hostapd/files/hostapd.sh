48cda63dc538982b5d51160780ec4ce7bb14af42
John Crispin <john@phrozen.org>
2023-09-14 08:06:02 +0200

    hostapd: backport latest reload patches
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/wifi-ax/hostapd/files/hostapd.sh
+++ feeds/wifi-ax/hostapd/files/hostapd.sh
@@ -725,8 +725,7 @@ hostapd_set_bss_options() {
 		[ -n "$wpa_strict_rekey" ] && append bss_conf "wpa_strict_rekey=$wpa_strict_rekey" "$N"
 	}
 
-	set_default nasid "${macaddr//\:}"
-	append bss_conf "nas_identifier=$nasid" "$N"
+	[ -n "$nasid" ] && append bss_conf "nas_identifier=$nasid" "$N"
 
 	[ -n "$acct_server" ] && {
 		append bss_conf "acct_server_addr=$acct_server" "$N"
