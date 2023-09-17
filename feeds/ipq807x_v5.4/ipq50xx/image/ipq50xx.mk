24d0fbfafa6c02f8062b4a93ba69f946195776c4
John Crispin <john@phrozen.org>
2023-09-17 07:15:28 +0200

    ipq807x: add cig-wf186w support to v5.4 kernel
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
+++ feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
@@ -1,5 +1,13 @@
 KERNEL_LOADADDR := 0x41080000
 
+define Device/cig_wf186w
+  DEVICE_TITLE := Cigtech WF-186w
+  DEVICE_DTS := qcom-ipq5018-cig-wf186w
+  SUPPORTED_DEVICES := cig,wf186w
+  DEVICE_PACKAGES := ath11k-wifi-cig-wf186w ath11k-firmware-ipq50xx-map-spruce
+  DEVICE_DTS_CONFIG := config@mp03.3
+endef
+TARGET_DEVICES += cig_wf186w
 
 define Device/edgecore_eap104
   DEVICE_TITLE := EdgeCore EAP104
