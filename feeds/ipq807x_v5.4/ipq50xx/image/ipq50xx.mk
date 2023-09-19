a913888c603c0e189c310fe81883b043c4d86b6a
Tanya Singh <tanya_singh@accton.com>
2023-09-19 13:39:49 +0800

    ipq50xx: Support OAP101 and OAP101e-6E (update board files for OAP101e and OAP101e-6E)
    Fixes: WIFI-12943
    Signed-off-by: Tanya Singh <tanya_singh@accton.com>

--- feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
+++ feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
@@ -40,7 +40,7 @@ define Device/edgecore_oap101_6e
   DEVICE_TITLE := EdgeCore OAP101 6E
   DEVICE_DTS := qcom-ipq5018-oap101-6e
   SUPPORTED_DEVICES := edgecore,oap101-6e
-  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-gpio-pca953x ugps
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-hwmon-tmp102 kmod-gpio-pca953x ugps kmod-tpm-tis-i2c
   DEVICE_DTS_CONFIG := config@mp03.5-c1
 endef
 TARGET_DEVICES += edgecore_oap101_6e
@@ -49,16 +49,16 @@ define Device/edgecore_oap101e
   DEVICE_TITLE := EdgeCore OAP101 E
   DEVICE_DTS := qcom-ipq5018-oap101e
   SUPPORTED_DEVICES := edgecore,oap101e
-  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-gpio-pca953x ugps
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101e ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-hwmon-tmp102 kmod-gpio-pca953x ugps kmod-tpm-tis-i2c
   DEVICE_DTS_CONFIG := config@mp03.5-c1
 endef
 TARGET_DEVICES += edgecore_oap101e
 
 define Device/edgecore_oap101e_6e
-  DEVICE_TITLE := EdgeCore OAP101e 6E
+  DEVICE_TITLE := EdgeCore OAP101 6E E
   DEVICE_DTS := qcom-ipq5018-oap101e-6e
   SUPPORTED_DEVICES := edgecore,oap101e-6e
-  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101 ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-hwmon-tmp102 kmod-gpio-pca953x ugps kmod-tpm-tis-i2c
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap101e ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122 kmod-hwmon-tmp102 kmod-gpio-pca953x ugps kmod-tpm-tis-i2c
   DEVICE_DTS_CONFIG := config@mp03.5-c1
 endef
 TARGET_DEVICES += edgecore_oap101e_6e
