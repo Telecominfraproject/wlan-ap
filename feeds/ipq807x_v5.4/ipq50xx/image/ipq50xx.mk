d6f0a1a2cf5397881957c4f36b1aaf912d5898a3
Ken <xshi@actiontec.com>
2023-11-02 10:27:05 +0800

    ipq50xx: CIG-WF186h Kernel/Firmware upgrade
    Upgrade to 5018 ath11k 12.0 wireless driver +  kernel 5.4
    
    Fixes: WIFI-13038
    Signed-off-by: Ken <xshi@actiontec.com>

--- feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
+++ feeds/ipq807x_v5.4/ipq50xx/image/ipq50xx.mk
@@ -1,10 +1,19 @@
 KERNEL_LOADADDR := 0x41080000
 
+define Device/cig_wf186h
+  DEVICE_TITLE := Cigtech WF-186h
+  DEVICE_DTS := qcom-ipq5018-cig-wf186h
+  SUPPORTED_DEVICES := cig,wf186h
+  DEVICE_PACKAGES := ath11k-wifi-cig-wf186h ath11k-firmware-ipq50xx-map-spruce
+  DEVICE_DTS_CONFIG := config@mp03.3
+endef
+TARGET_DEVICES += cig_wf186h
+
 define Device/cig_wf186w
   DEVICE_TITLE := Cigtech WF-186w
   DEVICE_DTS := qcom-ipq5018-cig-wf186w
   SUPPORTED_DEVICES := cig,wf186w
-  DEVICE_PACKAGES := ath11k-wifi-cig-wf186w ath11k-firmware-ipq50xx-map-spruce kmod-switch-rtl8367c
+  DEVICE_PACKAGES := ath11k-wifi-cig-wf186w ath11k-firmware-ipq50xx-map-spruce
   DEVICE_DTS_CONFIG := config@mp03.3
 endef
 TARGET_DEVICES += cig_wf186w
