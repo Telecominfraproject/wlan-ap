04f10325a1109d9f3da4025a6a4b469fc12acf32
John Crispin <john@phrozen.org>
2023-11-14 11:00:06 +0100

    switch-fabric: fix port mirroring
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ucentral/switch-fabric/files/usr/libexec/switch.sh
+++ feeds/ucentral/switch-fabric/files/usr/libexec/switch.sh
@@ -3,20 +3,17 @@
 . /lib/functions.sh
 
 port_mirror() {
-	for lan in $(ls -d /sys/class/net/lan* | cut -dn -f3 |sort -n); do
-		tc qdisc del dev lan$lan clsact
-	done
-
 	monitor=$(uci get switch.mirror.monitor)
 	analysis=$(uci get switch.mirror.analysis)
 
 	[ -n "$monitor" -a -n "$analysis" ] || return
+	tc qdisc del dev $analysis clsact
 
-	ifconfig $analysis up
 	for port in $monitor; do
+		tc qdisc del dev $port clsact
 		tc qdisc add dev $port clsact
-		tc filter add dev $port ingress matchall skip_sw action mirred egress mirror dev $analysis
-		tc filter add dev $port egress matchall skip_sw action mirred egress mirror dev $analysis
+		tc filter add dev $port ingress matchall action mirred egress mirror dev $analysis
+		tc filter add dev $port egress matchall action mirred egress mirror dev $analysis
 	done
 }
 
