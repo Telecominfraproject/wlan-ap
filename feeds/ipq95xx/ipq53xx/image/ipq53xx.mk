2f242fb40429bc9d2c0ebc362e41373884f3fc58
John Crispin <john@phrozen.org>
2024-01-23 11:41:48 +0100

    ipq95xx: various updates
    * finalize CIG WF198 support
    * add Sercomm AP72tip support
    * update BDF files
    * improve firmware package
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ipq95xx/ipq53xx/image/ipq53xx.mk
+++ feeds/ipq95xx/ipq53xx/image/ipq53xx.mk
@@ -8,6 +8,18 @@ define Device/cig_wf198
   IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
   IMAGE/nand-factory.bin := append-ubi | qsdk-ipq-factory-nand
   IMAGE/nand-factory.ubi := append-ubi
-  DEVICE_PACKAGES := ath12k-wifi-cig-wf198
+  DEVICE_PACKAGES := ath12k-wifi-cig-wf198 ath12k-firmware-qcn92xx-split-phy ath12k-firmware-ipq53xx
 endef
 TARGET_DEVICES += cig_wf198
+
+define Device/sercomm_ap72tip
+  DEVICE_TITLE := Sercomm AP72 TIP
+  DEVICE_DTS := ipq5332-sercomm-ap72tip
+  DEVICE_DTS_CONFIG := config@mi01.2-qcn9160-c1
+  IMAGES := sysupgrade.tar nand-factory.bin nand-factory.ubi
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
+  IMAGE/nand-factory.bin := append-ubi | qsdk-ipq-factory-nand
+  IMAGE/nand-factory.ubi := append-ubi
+  DEVICE_PACKAGES := ath12k-wifi-sercomm-ap72tip ath12k-firmware-qcn92xx-split-phy ath12k-firmware-ipq53xx
+endef
+TARGET_DEVICES += sercomm_ap72tip
