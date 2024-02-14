edebe60e9b32aa431d5cb884836cab7d18e0e067
John Crispin <john@phrozen.org>
2024-02-14 12:57:42 +0100

    ipq50xx: add CIF WF-198 support
    Signed-off-by: John Crispin <john@phrozen.org>

---
+++ feeds/ipq95xx/ipq53xx/base-files/lib/upgrade/platform.sh
@@ -0,0 +1,24 @@
+. /lib/functions/system.sh
+
+RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+platform_check_image() {
+	local magic_long="$(get_magic_long "$1")"
+	[ "$magic_long" = "73797375" ] && return 0
+	return 1
+}
+
+platform_do_upgrade() {
+	CI_UBIPART="rootfs"
+	CI_ROOTPART="ubi_rootfs"
+	CI_IPQ807X=1
+
+	board=$(board_name)
+	case $board in
+	qcom,ipq9574-ap-al02-c4|\
+	qcom,ipq9574-ap-al02-c15)
+		nand_upgrade_tar "$1"
+		;;
+	esac
+}
