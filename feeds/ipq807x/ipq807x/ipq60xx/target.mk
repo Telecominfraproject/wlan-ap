de425e8f17a02ce067b807779a16477dca0221eb
John Crispin <john@phrozen.org>
2024-05-01 11:58:59 +0200

    ipq807x: drop v4.4 kernel
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ipq807x/ipq807x/ipq60xx/target.mk
+++
@@ -1,8 +0,0 @@
-SUBTARGET:=ipq60xx
-BOARDNAME:=IPQ60xx based boards
-
-DEFAULT_PACKAGES += ath11k-firmware-ipq60xx qca-nss-fw-ipq60xx
-
-define Target/Description
-        Build images for IPQ60xx systems.
-endef
