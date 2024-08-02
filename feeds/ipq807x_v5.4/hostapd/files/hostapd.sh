0c19ddddcae78d00efce1e43edd77341f869be85
John Crispin <john@phrozen.org>
2024-08-02 11:06:37 +0200

    hostapd: fix wpa3-192 + 11r AKM
    Fixes: WIFI-14016
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/ipq807x_v5.4/hostapd/files/hostapd.sh
+++ feeds/ipq807x_v5.4/hostapd/files/hostapd.sh
@@ -47,8 +47,8 @@ hostapd_append_wpa_key_mgmt() {
 			[ "${ieee80211w:-0}" -gt 0 ] && append wpa_key_mgmt "WPA-${auth_type_l}-SHA256"
 		;;
 		eap192)
-			append wpa_key_mgmt "WPA-EAP-SUITE-B-192"
-			[ "${ieee80211r:-0}" -gt 0 ] && append wpa_key_mgmt "FT-EAP"
+			[ "${ieee80211r:-0}" -gt 0 ] || append wpa_key_mgmt "WPA-EAP-SUITE-B-192"
+			[ "${ieee80211r:-0}" -gt 0 ] && append wpa_key_mgmt "FT-EAP-SHA384"
 		;;
 		eap-eap2)
 			append wpa_key_mgmt "WPA-EAP"
