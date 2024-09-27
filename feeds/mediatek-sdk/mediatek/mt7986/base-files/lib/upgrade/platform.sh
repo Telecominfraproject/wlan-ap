d518e5988d3820d106a7d143d5a5d8392185dbfc
Steven Lin <steven.lin@senao.com>
2024-09-27 15:41:28 +0200

    mediatek: Add SENAO IAP4300M
    Specifications:
    SoC: MediaTek MT7986A
    RF Chipset: MT7976G @2.4GHz 4T4R
    MT7976A @5ghz 4T4R
    RAM: 1GB DDR4 RAM (2x 512MB)
    Flash: SPI-NAND 256 MiB
    Ethernet: 1x 2.5GbE PHY
    Reset Button
    Power Source: DC IN 12v, Standard PoE 802.3af/at
    LED Indicator: 4 x single color LED (GPIO Control)
    
    Fixes: WIFI-13983
    Signed-off-by: Steven Lin <steven.lin@senao.com>

--- feeds/mediatek-sdk/mediatek/mt7986/base-files/lib/upgrade/platform.sh
+++ feeds/mediatek-sdk/mediatek/mt7986/base-files/lib/upgrade/platform.sh
@@ -1,6 +1,18 @@
 RAMFS_COPY_BIN='mkfs.f2fs blkid blockdev fw_printenv fw_setenv dmsetup'
 RAMFS_COPY_DATA="/etc/fw_env.config /var/lock/fw_printenv.lock"
 
+senao_swap_active_fw() {
+	echo "Doing swap active_fw" > /dev/console
+	tmp_active_fw=$(fw_printenv -n active_fw)
+	if [ $tmp_active_fw == "0" ]; then
+		fw_setenv active_fw 1
+		fw_setenv mtdparts nmbm0:1024k\(bl2\),512k\(u-boot-env\),2048k\(factory\),2048k\(fip\),112640k\(ubi_1\),112640k\(ubi\),384k\(cert\),640k\(userconfig\),384k\(crashdump\)
+	else
+		fw_setenv active_fw 0
+		fw_setenv mtdparts nmbm0:1024k\(bl2\),512k\(u-boot-env\),2048k\(factory\),2048k\(fip\),112640k\(ubi\),112640k\(ubi_1\),384k\(cert\),640k\(userconfig\),384k\(crashdump\)
+	fi
+}
+
 platform_do_upgrade() {
 	local board=$(board_name)
 
@@ -10,6 +22,10 @@ platform_do_upgrade() {
 		;;
 	*emmc*)
 		mtk_mmc_do_upgrade "$1"
+		;;
+	senao,iap4300m)
+		CI_UBIPART="ubi_1"
+		nand_do_upgrade "$1"
 		;;
 	*)
 		default_do_upgrade "$1"
@@ -26,6 +42,7 @@ platform_check_image() {
 	[ "$#" -gt 1 ] && return 1
 
 	case "$board" in
+	senao,iap4300m |\
 	*snand* |\
 	*emmc*)
 		# tar magic `ustar`
@@ -48,4 +65,14 @@ platform_check_image() {
 	esac
 
 	return 0
+}
+
+platform_post_upgrade_success() {
+	local board=$(board_name)
+
+	case "$board" in
+		senao,iap4300m)
+			senao_swap_active_fw
+		;;
+	esac
 }
