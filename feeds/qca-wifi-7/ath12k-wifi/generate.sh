dbdc16804dd090b1aa2eca4642bb8ebf8c9b350c
John Crispin <john@phrozen.org>
2025-02-27 15:54:21 +0100

    ipq5322: update rap7110c_341x to ATH12.5.5
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/qca-wifi-7/ath12k-wifi/generate.sh
+++ feeds/qca-wifi-7/ath12k-wifi/generate.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# https://github.com/qca/qca-swiss-army-knife.git
+
 encoder=/code/qca/qca-swiss-army-knife/tools/scripts/ath12k/ath12k-bdencoder
 
 $encoder -c board-2-eap105-IPQ5332.json -o board-2.bin.eap105.IPQ5332
 $encoder -c board-2-eap105-QCN92XX.json -o board-2.bin.eap105.QCN92XX
+
+$encoder -c board-2-rap7110c_341x-IPQ5332.json -o board-2.bin.rap7110c_341x.IPQ5332
+$encoder -c board-2-rap7110c_341x-QCN92XX.json -o board-2.bin.rap7110c_341x.QCN92XX
