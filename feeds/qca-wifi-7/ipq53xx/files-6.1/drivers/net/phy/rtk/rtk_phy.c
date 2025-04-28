df001072b755b0c9ec918b5f803f5d9522f96559
Ken <xshi@actiontec.com>
2025-04-28 13:55:49 +0800

    WIFI-14546 WF189   ETH port issue start from 12.5
    Signed-off-by: Ken <xshi@actiontec.com>

--- feeds/qca-wifi-7/ipq53xx/files-6.1/drivers/net/phy/rtk/rtk_phy.c
+++ feeds/qca-wifi-7/ipq53xx/files-6.1/drivers/net/phy/rtk/rtk_phy.c
@@ -48,6 +48,9 @@ static int rtl826xb_get_features(struct phy_device *phydev)
     linkmode_clear_bit(ETHTOOL_LINK_MODE_10baseT_Full_BIT,
                        phydev->supported);
 
+    linkmode_clear_bit(ETHTOOL_LINK_MODE_10000baseT_Full_BIT,
+                       phydev->supported);
+
     return 0;
 }
 
@@ -80,7 +83,6 @@ static int rtkphy_config_init(struct phy_device *phydev)
         case REALTEK_PHY_ID_RTL8261N:
         case REALTEK_PHY_ID_RTL8264B:
             phydev_info(phydev, "%s:%u [RTL8261N/RTL826XB] phy_id: 0x%X PHYAD:%d\n", __FUNCTION__, __LINE__, phydev->drv->phy_id, phydev->mdio.addr);
-	    phy_modify_mmd_changed(phydev, 7, 0x20, BIT(12), 0);
 
           #if 1 /* toggle reset */
             phy_modify_mmd_changed(phydev, 30, 0x145, BIT(0)  , 1);
@@ -213,7 +215,6 @@ static int rtkphy_c45_aneg_done(struct phy_device *phydev)
 static int rtkphy_c45_read_status(struct phy_device *phydev)
 {
     int ret = 0, status = 0;
-    uint16_t local;
     phydev->speed = SPEED_UNKNOWN;
     phydev->duplex = DUPLEX_UNKNOWN;
     phydev->pause = 0;
@@ -232,9 +233,6 @@ static int rtkphy_c45_read_status(struct phy_device *phydev)
         if (ret)
             return ret;
 	
-	phy_write_mmd(phydev, 7, 0x20, 0x181);
-	local = phy_read_mmd(phydev, 7, 0x20);
-
         status =  phy_read_mmd(phydev, 31, 0xA414);
         if (status < 0)
             return status;
@@ -242,11 +240,6 @@ static int rtkphy_c45_read_status(struct phy_device *phydev)
             phydev->lp_advertising, status & BIT(11));
 
         phy_resolve_aneg_linkmode(phydev);
-	if((phydev->speed == 10000) && (local == 0x181))
-        {
-                phydev->speed = 5000;
-                phydev->duplex = DUPLEX_FULL;
-        }
     }
     else
     {
