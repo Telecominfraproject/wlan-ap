9e5d121cd9b48eb4d01fbba368884489db40ae36
mike_ding <mike_ding@sdc.sercomm.com>
2025-05-27 10:38:07 +0800

    Support dual image and fix reset button does not work issue
    Signed-off-by: mike_ding <mike_ding@sdc.sercomm.com>

--- feeds/qca-wifi-7/ipq53xx/base-files/lib/upgrade/platform.sh
+++ feeds/qca-wifi-7/ipq53xx/base-files/lib/upgrade/platform.sh
@@ -86,6 +86,8 @@ platform_do_upgrade() {
 
 	board=$(board_name)
 	case $board in
+	sercomm,ap72tip-v4|\
+	sercomm,ap72tip|\
 	cig,wf189w|\
 	cig,wf189h|\
 	cig,wf189)
@@ -119,10 +121,6 @@ platform_do_upgrade() {
 	sonicfi,rap750e-h|\
 	sonicfi,rap750w-311a)
 		sonicfi_dualimage_check
-		nand_upgrade_tar "$1"
-		;;
-	sercomm,ap72tip-v4|\
-	sercomm,ap72tip)
 		nand_upgrade_tar "$1"
 		;;
 	zyxel,nwa130be)
