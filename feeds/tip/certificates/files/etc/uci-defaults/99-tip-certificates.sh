842b21fb5e6f16a423b5a8808d46503777a87173
John Crispin <john@phrozen.org>
2025-07-08 09:40:06 +0200

    certificates: add an explicit uci commit call
    Signed-off-by: John Crispin <john@phrozen.org>

--- feeds/tip/certificates/files/etc/uci-defaults/99-tip-certificates.sh
+++ feeds/tip/certificates/files/etc/uci-defaults/99-tip-certificates.sh
@@ -4,3 +4,4 @@ uci add system certificates
 uci set system.@certificates[-1].key=/etc/ucentral/key.pem
 uci set system.@certificates[-1].cert=/etc/ucentral/operational.pem
 uci set system.@certificates[-1].ca=/etc/ucentral/operational.ca
+uci commit
