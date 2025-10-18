c82807604948e06c584f6c050acc44ef44d344c3
John Crispin <john@phrozen.org>
2025-10-19 00:00:00 +0200

    feeds: rename ipq807x_v5.4 to qca-wifi-6
    Rename the ipq807x_v5.4 feed to qca-wifi-6 to align with the naming
    convention used for qca-wifi-7. This change improves consistency and
    better reflects the content, which encompasses WiFi 6 (802.11ax)
    packages across various Qualcomm IPQ platforms (IPQ5018, IPQ6018,
    IPQ8074, etc.).
    
    Signed-off-by: John Crispin <john@phrozen.org>

---
+++ feeds/qca-wifi-6/ipq807x/image/ipq807x.mk
@@ -0,0 +1,113 @@
+KERNEL_LOADADDR := 0x41080000
+
+define Device/cig_wf194c4
+  DEVICE_TITLE := CIG WF194C4
+  DEVICE_DTS := qcom-ipq807x-wf194c4
+  DEVICE_DTS_CONFIG=config@hk09
+  SUPPORTED_DEVICES := cig,wf194c4
+  DEVICE_PACKAGES := ath11k-wifi-cig-wf194c4 aq-fw-download uboot-envtools kmod-usb3 kmod-usb2
+endef
+TARGET_DEVICES += cig_wf194c4
+
+define Device/cig_wf196
+  DEVICE_TITLE := CIG WF196
+  DEVICE_DTS := qcom-ipq807x-wf196
+  DEVICE_DTS_CONFIG=config@hk14
+  SUPPORTED_DEVICES := cig,wf196
+  BLOCKSIZE := 256k
+  PAGESIZE := 4096
+  DEVICE_PACKAGES := ath11k-wifi-cig-wf196 aq-fw-download uboot-envtools kmod-usb3 kmod-usb2 \
+  	ath11k-firmware-qcn9000
+endef
+TARGET_DEVICES += cig_wf196
+
+define Device/edgecore_eap102
+  DEVICE_TITLE := Edgecore EAP102
+  DEVICE_DTS := qcom-ipq807x-eap102
+  DEVICE_DTS_CONFIG=config@ac02
+  SUPPORTED_DEVICES := edgecore,eap102
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-eap102 kmod-usb2 kmod-usb3 uboot-envtools
+endef
+TARGET_DEVICES += edgecore_eap102
+
+define Device/edgecore_oap102
+  DEVICE_TITLE := Edgecore OAP102
+  DEVICE_DTS := qcom-ipq807x-oap102
+  DEVICE_DTS_CONFIG=config@ac02
+  SUPPORTED_DEVICES := edgecore,oap102
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-eap102 kmod-usb2 kmod-usb3 uboot-envtools
+endef
+TARGET_DEVICES += edgecore_oap102
+
+define Device/edgecore_oap103
+  DEVICE_TITLE := Edgecore OAP103
+  DEVICE_DTS := qcom-ipq807x-oap103
+  DEVICE_DTS_CONFIG=config@ac02
+  SUPPORTED_DEVICES := edgecore,oap103
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-oap103 kmod-usb2 kmod-usb3 uboot-envtools
+endef
+TARGET_DEVICES += edgecore_oap103
+
+define Device/edgecore_eap106
+  DEVICE_TITLE := Edgecore EAP106
+  DEVICE_DTS := qcom-ipq807x-eap106
+  DEVICE_DTS_CONFIG=config@hk02
+  SUPPORTED_DEVICES := edgecore,eap106
+  DEVICE_PACKAGES := ath11k-wifi-edgecore-eap106 aq-fw-download kmod-usb2 kmod-usb3 uboot-envtools
+endef
+#TARGET_DEVICES += edgecore_eap106
+
+define Device/emplus_wap380c
+  DEVICE_TITLE := Emplus WAP380C
+  DEVICE_DTS := qcom-ipq807x-wap380c
+  DEVICE_DTS_CONFIG=config@hk07
+  SUPPORTED_DEVICES := emplus,wap380c
+  DEVICE_PACKAGES := ath11k-wifi-emplus-wap380c uboot-envtools
+endef
+TARGET_DEVICES += emplus_wap380c
+
+define Device/indio_um-345ax
+  DEVICE_TITLE := Indio UM-345AX
+  DEVICE_DTS := qcom-ipq807x-indio-um-345ax
+  DEVICE_DTS_CONFIG=config@hk09
+  SUPPORTED_DEVICES := indio,um-345ax
+  DEVICE_PACKAGES := ath11k-wifi-indio-um-345ax uboot-envtools
+endef
+TARGET_DEVICES += indio_um-345ax
+
+define Device/sonicfi_rap650c
+  DEVICE_TITLE := SonicFi RAP650C
+  DEVICE_DTS := qcom-ipq807x-rap650c
+  DEVICE_DTS_CONFIG=config@hk09
+  SUPPORTED_DEVICES := sonicfi,rap650c
+  DEVICE_PACKAGES := ath11k-wifi-sonicfi-rap650c uboot-envtools
+  IMAGES := sysupgrade.bin nand-factory.bin nand-factory.ubi
+  IMAGE/nand-factory.ubi := append-ubi
+endef
+TARGET_DEVICES += sonicfi_rap650c
+
+define Device/tplink_ex227
+  DEVICE_TITLE := TP-Link EX227
+  DEVICE_DTS := qcom-ipq807x-ex227
+  DEVICE_DTS_CONFIG=config@hk07
+  SUPPORTED_DEVICES := tplink,ex227
+  DEVICE_PACKAGES := ath11k-wifi-tplink-ex227
+  IMAGES := sysupgrade.bin nand-factory.bin nand-factory.ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/nand-factory.bin := append-ubi | qsdk-ipq-factory-nand
+  IMAGE/nand-factory.ubi := append-ubi
+endef
+TARGET_DEVICES += tplink_ex227
+
+define Device/tplink_ex447
+  DEVICE_TITLE := TP-Link EX447
+  DEVICE_DTS := qcom-ipq807x-ex447
+  DEVICE_DTS_CONFIG=config@hk09
+  SUPPORTED_DEVICES := tplink,ex447
+  DEVICE_PACKAGES := ath11k-wifi-tplink-ex447
+  IMAGES := sysupgrade.bin nand-factory.bin nand-factory.ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/nand-factory.bin := append-ubi | qsdk-ipq-factory-nand
+  IMAGE/nand-factory.ubi := append-ubi
+endef
+TARGET_DEVICES += tplink_ex447
