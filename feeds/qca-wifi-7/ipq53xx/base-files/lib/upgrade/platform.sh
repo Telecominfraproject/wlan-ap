ebf2d9bf92a7de6504f1d2416724982a1cae0f58
Venkat Chimata <venkat@nearhop.com>
2026-01-27 19:32:16 +0530

    qca-wifi-7: Fix a syntax error in platform.sh script.
    There was an addition fi.
    Remove it and indent the if block correctly.
    
    Signed-off-by: Venkat Chimata <venkat@nearhop.com>

--- feeds/qca-wifi-7/ipq53xx/base-files/lib/upgrade/platform.sh
+++ feeds/qca-wifi-7/ipq53xx/base-files/lib/upgrade/platform.sh
@@ -152,29 +152,28 @@ platform_do_upgrade() {
 		;;
 	edgecore,eap105)
 		if [ -e /tmp/downgrade ]; then
+			CI_UBIPART="rootfs1"
+			{ echo 'active 1'; echo 'upgrade_available 0'; } > /tmp/fw_setenv.txt || exit 1
+			CI_FWSETENV="-s /tmp/fw_setenv.txt"
+		else
+			local CI_UBIPART_B=""
+			if grep -q rootfs1 /proc/cmdline; then
+				CI_UBIPART="rootfs2"
+				CI_UBIPART_B="rootfs1"
+				CI_FWSETENV="active 2"
+			elif grep -q rootfs2 /proc/cmdline; then
 				CI_UBIPART="rootfs1"
-				{ echo 'active 1'; echo 'upgrade_available 0'; } > /tmp/fw_setenv.txt || exit 1
-				CI_FWSETENV="-s /tmp/fw_setenv.txt"
+				CI_UBIPART_B="rootfs2"
+				CI_FWSETENV="active 1"
 			else
-				local CI_UBIPART_B=""
-				if grep -q rootfs1 /proc/cmdline; then
-					CI_UBIPART="rootfs2"
-					CI_UBIPART_B="rootfs1"
-					CI_FWSETENV="active 2"
-				elif grep -q rootfs2 /proc/cmdline; then
-					CI_UBIPART="rootfs1"
-					CI_UBIPART_B="rootfs2"
-					CI_FWSETENV="active 1"
-				else
-					CI_UBIPART="rootfs1"
-					CI_UBIPART_B=""
-					CI_FWSETENV="active 1"
-				fi
-				if [ "$(fw_printenv -n upgrade_available 2>/dev/null)" = "0" ]; then
-					if [ -n "$CI_UBIPART_B" ]; then
-						CI_UBIPART="$CI_UBIPART_B"
-						CI_FWSETENV=""
-					fi
+				CI_UBIPART="rootfs1"
+				CI_UBIPART_B=""
+				CI_FWSETENV="active 1"
+			fi
+			if [ "$(fw_printenv -n upgrade_available 2>/dev/null)" = "0" ]; then
+				if [ -n "$CI_UBIPART_B" ]; then
+					CI_UBIPART="$CI_UBIPART_B"
+					CI_FWSETENV=""
 				fi
 			fi
 		fi
